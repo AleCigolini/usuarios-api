2f4860d7aed9e8807ca33ed2b9654bf1e05a1b72
AleCigolini <cigolini.ale@gmail.com>
2025-09-29 21:46:16 -0300

    Ajustado deploy

--- src/main/java/br/com/fiap/techchallenge/api/usuario/application/usecase/impl/ValidarSenhaUsuarioUseCaseImpl.java
+++ src/main/java/br/com/fiap/techchallenge/api/usuario/application/usecase/impl/ValidarSenhaUsuarioUseCaseImpl.java
@@ -4,12 +4,16 @@ import br.com.fiap.techchallenge.api.usuario.application.usecase.ValidarSenhaUsu
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.logging.Logger;
+
 @AllArgsConstructor
 public class ValidarSenhaUsuarioUseCaseImpl implements ValidarSenhaUsuarioUseCase {
     private final PasswordEncoder passwordEncoder;
+    private static final Logger LOGGER = Logger.getLogger(ValidarSenhaUsuarioUseCaseImpl.class.getName());
 
     @Override
     public Boolean isSenhaValida(String senhaSalva, String senhaInformada) {
+        LOGGER.info("Validando senha");
         return passwordEncoder.matches(senhaInformada, senhaSalva);
     }
 }
